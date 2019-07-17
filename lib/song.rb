e12962b4600d3281da66f4010d066ca5b8ae3a1e
HyoEun Lee <hyoeun93@gmail.com>
2019-07-17 12:47:02 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,16 @@
+class Song 
+  attr_accessor :name, :artist, :genre 
+  
+  @@count = 0 
+  
+  def initialize(name, artist, genre)
+    @@count += 1
+  end 
+  
+  def self.count
+    @@count
+  end 
+  
+  def self.genres 
+  end 
+end 
